0152ef10877f24143becf8f35619e7bd38ff0e44
Elvis-rozy <elvisrozy52@gmail.com>
2022-10-18 07:47:57 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print all alphabet in lowercase 10 times
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  */
 
 void print_alphabet_x10(void)
