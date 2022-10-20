90e237532382be33b229e3f1c92a4aaf2cd0cf1b
Elvis-rozy <elvisrozy52@gmail.com>
2022-10-20 07:27:51 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal - printing diagonal lines
+ * @n: number of times
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar('\');
+	}
+	_putchar('\n');
+}
