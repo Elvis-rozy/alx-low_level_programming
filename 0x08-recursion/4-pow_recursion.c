a5b1e3be757f6614272ae3ff1c3f3da009eb30e8
Elvis-rozy <elvisrozy52@gmail.com>
2022-11-02 08:35:41 -0700

    mandatory tasks done

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,15 +9,13 @@
 
 int _pow_recursion(int x, int y)
 {
-	int b = (x * _pow_recursion(x, y - 1));
-
 	if (y < 0)
 	{
 		return (-1);
 	}
 	else if (y != 0)
 	{
-		return (b);
+		return (x * _pow_recursion(x, y - 1));
 	}
 	else
 	{
