1ad05bcb7261bcab965e4267e7777f9e35b601cb
Elvis-rozy <elvisrozy52@gmail.com>
2022-11-04 06:13:07 -0700

    task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,11 @@
-#include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 /**
  * main - prints its name, followed by a new line
  * @argc: argument count
  * @argv: argument vector
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	(void)argc;
 
