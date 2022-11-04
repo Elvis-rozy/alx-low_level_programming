afab5bca756e397be6737fcc5b7c724b40ba5d6b
Elvis-rozy <elvisrozy52@gmail.com>
2022-11-04 12:44:46 -0700

    g

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,17 +2,17 @@
 
 /**
  * _puts - prints a string
- * @str: the string
+ * @s: the string
  *
  * Return: the length of the string
  */
-void _puts(char *str)
+void _puts(char *s)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		_putchar(str[i]);
+		_putchar(s[i]);
 		i++;
 	}
 	_putchar('\n');
