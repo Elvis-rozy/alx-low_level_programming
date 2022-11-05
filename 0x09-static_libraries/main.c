efee86914f3d80f9a774670d97933ca9761f965a
Elvis-rozy <elvisrozy52@gmail.com>
2022-11-04 17:50:50 -0700

    d

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,11 @@
 #include "main.h"
-
+/**
+ * main - entry point
+ * Return: 0
+ */
 int main(void)
 {
 
-	-puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
