bb51023a8b05b70e4e4f717b7c191c1e172cda2f
Elvis-rozy <elvisrozy52@gmail.com>
2022-11-04 17:23:59 -0700

    d

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,13 +1,8 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * main - prints output
- * Return: 0
- */
+
 int main(void)
 {
-	printf("\"\"At the end of the day, ");
-	printf("my goal was to be the best hacker\"\"\n\"\t- Kevin Mitnick\n");
 
+	-puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
