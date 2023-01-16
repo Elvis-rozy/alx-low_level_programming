f242b8acfad294bf05d95d14e7b4b04d6c219e1c
Elvis-rozy <elvisrozy52@gmail.com>
2023-01-16 08:22:33 -0800

    done

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings.
+ * @src: the source string to append to @dest
+ * @dest: the destiation string to be concatenated upon
+ * Return: dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+        int i;
+        int j;
+
+        i = 0;
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
+        j = 0;
+        while (src[j] != '\0')
+        {
+                dest[i] = src[j];
+                i++;
+                j++;
+        }
+        dest[i] = '\0';
+        return (dest);
+}
