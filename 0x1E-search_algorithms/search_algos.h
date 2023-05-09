c8b78e31793d2482a8a555e8957cfbc4ec1ce9bf
Elvis-rozy <elvisrozy52@gmail.com>
2023-05-09 12:48:07 -0700

    advanced section attempt 9

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,10 +1,12 @@
-#ifndef SEARCH_H
-#define SEARCH_H
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
 
+/*INCLUDED LIBRARIES */
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
+/* STRUCTS */
 /**
  * struct listint_s - singly linked list
  *
@@ -13,7 +15,6 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
@@ -21,51 +22,35 @@ typedef struct listint_s
 	size_t index;
 	struct listint_s *next;
 } listint_t;
-
-
-/*
- * Function that searches for a value in an
- * array of integers using the Linear search algorithm
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
  */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+/* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
-
-/*
- * Function that searches for a value in a sorted array
- * of integers using the Binary search algorithm
- */
 int binary_search(int *array, size_t size, int value);
-
-/*
- * Function that searches for a value in a
- * sorted array of integers using the Jump search algorithm
-*/
 int jump_search(int *array, size_t size, int value);
-
-/*
- * Function that searches for a value in a sorted array of
- * integers using the Interpolation search algorithm
-*/
 int interpolation_search(int *array, size_t size, int value);
-
-/*
- * Function that searches for a value in a sorted array
- * of integers using the Exponential search algorithm
- */
 int exponential_search(int *array, size_t size, int value);
-
-/*
- * Function that searches for a value in a sorted list
- * of integers using the Jump search algorithm in a singly linked list
- */
+int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
-
- /* print_list - Prints the content of a listint_t */
-void print_list(const listint_t *list);
-
-/* create_list - Creates a single linked list */
 listint_t *create_list(int *array, size_t size);
-
-/* free_list - Deallocates a singly linked list */
 void free_list(listint_t *list);
+void print_list(const listint_t *list);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
-#endif /* SEARCH_H */
+#endif
